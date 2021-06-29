02a1732dbde65ee06daf320adfb151c097774062
Alexander Simakin <qq.siiimka@gmail.com>
2021-06-29 15:28:58 +1000

    Add main function logic

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,6 +1,35 @@
+import DigitsRepresentation from "./digitsRepresentation";
+import NumberArrayCounting from "./numberArrayCounting";
+
 class App {
     static main(args:string[]) : void {
+        if (args.length != 0) {
+            for (const arg in args) {
+                this.present(+arg);
+            }
+        } else {
+            let stdin = process.openStdin();
+            stdin.addListener("data", (data) => {
+                if (data.toString().trim() !== '') {
+                    try {
+                        this.present(data);
+                    } catch {
+                        process.exit();
+                    }
+                }
+            });
+        }
+    }
 
+    private static present(input: number): void {
+        if (isNaN(input)) {
+            throw new Error("invalid number format");
+        }
+        let digits = DigitsRepresentation.getDigitsArray(input);
+        console.log(`Avg: ${NumberArrayCounting.getAverage(digits)}`);
+        console.log(`Max: ${NumberArrayCounting.getMax(digits)}`);
+        console.log(`Min: ${NumberArrayCounting.getMin(digits)}`);
+        console.log(`Sum: ${NumberArrayCounting.getSum(digits)}`);
     }
 }
 
