e47368622107215ae40febd123cc004e31ebc49f
Alexander Simakin <qq.siiimka@gmail.com>
2021-07-01 20:36:44 +1000

    Add consists of numbers validation for numberArrayCounting

--- src/main/numberArrayCounting.ts
+++ src/main/numberArrayCounting.ts
@@ -1,27 +1,39 @@
 export default class NumberArrayCounting {
 
-    private static validateArrayLength(array: number[]) : void {
+    private static validateArrayNotEmpty(array: number[]) : void {
         if (array.length == 0) {
             throw new RangeError("array should not be empty");
         }
     }
 
+    private static validateArrayConsistsOfNumbers(array: number[]) : void {
+        for (let i = 0; i < array.length; i++) {
+            if (isNaN(array[i])) {
+                throw new TypeError("array must consist of numbers");
+            }
+        }
+    }
+
     static getAverage(array: number[]) : number {
-        this.validateArrayLength(array);
+        this.validateArrayNotEmpty(array);
+        this.validateArrayConsistsOfNumbers(array);
         return this.getSum(array) / array.length;
     }
 
     static getMax(array: number[]) : number {
-        this.validateArrayLength(array);
+        this.validateArrayNotEmpty(array);
+        this.validateArrayConsistsOfNumbers(array);
         return Math.max(...array);
     }
 
     static getMin(array: number[]) : number {
-        this.validateArrayLength(array);
+        this.validateArrayNotEmpty(array);
+        this.validateArrayConsistsOfNumbers(array);
         return Math.min(...array);
     }
 
     static getSum(array: number[]) : number {
+        this.validateArrayConsistsOfNumbers(array);
         return array.reduce((a, b) => a + b, 0);
     }
 
